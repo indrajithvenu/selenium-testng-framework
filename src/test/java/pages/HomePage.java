66d6aef2b82e90371d933d098220b1722cd0b3aa
indrajithvenu <indrajithvenu@gmail.com>
2026-05-27 12:27:17 +0530

    Added product search automation tests

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,68 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;	
+import java.util.List;
+import org.openqa.selenium.WebElement;
+
+
 public class HomePage {
+	
+	WebDriver driver;
+	
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	By searchBox = By.id("search-query");
+	By searchButton = By.cssSelector("[data-test='search-submit']");
+	By searchCaption = By.cssSelector("[data-test='search-caption']");
+	By noResult = By.cssSelector("[data-test='no-results']");
+	By productCards = By.cssSelector("[data-test='search_completed'] a[href*='/product/'][data-test^='product-']");
+	
+	
+	public void searchProduct(String productName) {
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(searchBox));
+		
+		driver.findElement(searchBox).sendKeys(productName);
+		driver.findElement(searchButton).click();
+		
+	}
+	
+	public String getSearchCaption() {
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(searchCaption));
+		
+		return driver.findElement(searchCaption).getText();
+
+	}
+	
+	public int getDisplayedProductCount() {
+		
+
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+	    wait.until(ExpectedConditions.visibilityOfElementLocated(productCards));	
+
+		List<WebElement> products = driver.findElements(productCards);
+
+		return products.size();
+		
+	}
+	
+	
+	public String getNoResultMessage() {
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(noResult));
+
+		return driver.findElement(noResult).getText(); 
+	}
 
 }
---
+++ src/test/java/tests/HomePageTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.BaseClass;
+import pages.HomePage;
+
+public class HomePageTest extends BaseClass{
+	
+	@Test
+	public void verifyValidProductSearch() {
+		
+		HomePage homePage = new HomePage(driver);
+		
+		homePage.searchProduct("saw");
+		
+		String caption = homePage.getSearchCaption();
+		int productCount = homePage.getDisplayedProductCount();
+		
+		
+		Assert.assertTrue(productCount > 0);			
+		Assert.assertTrue(caption.contains("saw"));
+		
+	}
+	
+	@Test
+	public void verifyInvalidProductSearch() {
+		
+		HomePage homePage = new HomePage(driver);
+		
+		homePage.searchProduct("xyz");
+		
+		String caption = homePage.getSearchCaption();
+		String noResultMessage = homePage.getNoResultMessage();
+		
+		
+		Assert.assertTrue(caption.contains("xyz"));
+		Assert.assertEquals(noResultMessage,"There are no products found.");
+		
+	}
+
+}
